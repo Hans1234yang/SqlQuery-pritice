3bc1e8bc421364f1dbc29d2b08ad30bf7f320196
Hans Yang <545872067@qq.com>
2019-03-08 09:39:08 +0800

    添加项目文件。

---
+++ Service/UserService.cs
@@ -0,0 +1,21 @@
+﻿using EF;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class UserService
+    {
+        MyTestEntities myEntity = new MyTestEntities();
+
+        public List<User> GetUserList()
+        {
+            string name = "hans";
+            return myEntity.Database.SqlQuery<User>("select * from Stu where userName=@nameeee", new SqlParameter("@nameeee", name) ).ToList();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        UserService userService = new UserService(); 
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var list= userService.GetUserList();
+            return View();
+        }
+    }
+}
